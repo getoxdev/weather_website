afd162400fb8ea62819c23dad97f079289772d44
getoxdev <getoxdev@gmail.com>
2020-03-31 01:30:09 +0530

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ searchForm.addEventListener('submit', (e) => {
     message1.textContent="Fetching Weather Details..."
     message2.textContent=''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 message1.textContent=data.error
